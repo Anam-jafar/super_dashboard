f8d02ef2c3ae60ba45bb1ed47b98f6c86a7f3985
Anam-jafar <anamibnjafar@gmail.com>
2025-02-24 08:02:41 +0000

    Subscription Controller

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -10,16 +10,27 @@ class SubscriptionController extends Controller
 {
 
 
-public function activeSubscriptions() 
-{
-    $subscriptions = DB::table('client')
-        ->where('isActivated', 1)
-        ->paginate(10);
+    public function activeSubscriptions() 
+    {
+        $subscriptions = DB::table('client')
+            ->where('isActivated', 1)
+            ->paginate(10);
 
-    $statuses = DB::table('type')->where('grp', 'clientstatus')->get();
+        $statuses = DB::table('type')->where('grp', 'clientstatus')->get();
 
 
-    return view('subscription.active_subscription', compact(['subscriptions', 'statuses']));
-}
+        return view('subscription.active_subscription', compact(['subscriptions', 'statuses']));
+    }
+
+    public function requestSubscriptions()
+    {
+        $subscriptions = DB::table('client')
+            ->where('opt1', 1)
+            ->paginate(10);
+        
+        return view('subscription.new_subscription_application', compact('subscriptions'));
+    }
+
+
 
 }

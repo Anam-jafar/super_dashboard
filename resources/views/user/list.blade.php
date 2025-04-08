eca73394e576ffd769fbb1caa45e8cef3a0eb1c8
Anam-jafar <anamibnjafar@gmail.com>
2025-04-08 14:27:50 +0800

    Changes in packages

--- resources/views/user/list.blade.php
+++ resources/views/user/list.blade.php
@@ -1,45 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="main-content app-content">
-        <div class="container-fluid">
+  <div class="main-content app-content">
+    <div class="container-fluid">
 
-            <x-page-header :title="'Senarai Rekod Pengguna'" :breadcrumbs="[
-                ['label' => 'Pengurusan Pengguna', 'url' => 'javascript:void(0);'],
-                ['label' => 'Senarai Pengguna'],
-            ]" />
-            <x-alert />
-            <div class="py-8 px-4 rounded-lg shadow bg-white">
+      <x-page-header :title="'Senarai Rekod Pengguna'" :breadcrumbs="[['label' => 'Pengurusan Pengguna', 'url' => 'javascript:void(0);'], ['label' => 'Senarai Pengguna']]" />
+      <x-alert />
+      <div class="rounded-lg bg-white px-4 py-8 shadow">
 
-                <x-filter-card :filters="[
-                    [
-                        'name' => 'syslevel',
-                        'label' => 'Semua Pengguna',
-                        'type' => 'select',
-                        'options' => $parameters['admin_groups'],
-                    ],
-                    [
-                        'name' => 'joblvl',
-                        'label' => 'Semua Akses',
-                        'type' => 'select',
-                        'options' => $parameters['districts'],
-                    ],
-                    ['name' => 'search', 'label' => 'Search by Name', 'type' => 'text', 'placeholder' => 'Carian...'],
-                ]" :route="route('userList')" button-label="Daftar Baru" :button-route="route('userCreate')" />
+        <x-filter-card :filters="[
+            [
+                'name' => 'syslevel',
+                'label' => 'Semua Pengguna',
+                'type' => 'select',
+                'options' => $parameters['admin_groups'],
+            ],
+            [
+                'name' => 'joblvl',
+                'label' => 'Semua Akses',
+                'type' => 'select',
+                'options' => $parameters['districts'],
+            ],
+            ['name' => 'search', 'label' => 'Search by Name', 'type' => 'text', 'placeholder' => 'Carian...'],
+        ]" :route="route('userList')" button-label="Daftar Baru" :button-route="route('userCreate')" />
 
-                <x-table :headers="[
-                    'Nama',
-                    'Emel',
-                    'No Kad Pengenalan',
-                    'No Telefon',
-                    'Peringkat Pengguna',
-                    'Akses Daerah',
-                    'Status',
-                ]" :columns="['name', 'mel', 'ic', 'hp', 'USER_GROUP', 'DISTRICT_ACCESS', 'STATUS']" :id="'id'" :rows="$users" route="userEdit" />
+        <x-table :headers="['Nama', 'Emel', 'No Kad Pengenalan', 'No Telefon', 'Peringkat Pengguna', 'Akses Daerah', 'Status']" :columns="['name', 'mel', 'ic', 'hp', 'USER_GROUP', 'DISTRICT_ACCESS', 'STATUS']" :id="'id'" :rows="$users" route="userEdit" />
 
-                <x-pagination :items="$users" label="jumlah rekod" />
+        <x-pagination :items="$users" label="jumlah rekod" />
 
-            </div>
-        </div>
+      </div>
     </div>
+  </div>
 @endsection
